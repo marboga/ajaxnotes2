675b3702bf3c96586112d042c933861864d65084
Michael Arbogast <marboga@gmail.com>
2016-06-27 14:56:59 -0700

    non ajax basic functionality

--- server.py
+++ server.py
@@ -1,18 +1,51 @@
-from flask import Flask, session, redirect, render_template, request
+from flask import Flask, session, redirect, render_template, request, jsonify
 from mysqlconnection import MySQLConnector
 
 app = Flask(__name__)
 app.secret_key = "sdghejrehwgafc"
-mysql = MySQLConnector(app, "ajax")
+mysql = MySQLConnector(app, "ajax_notes")
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	query = "SELECT * FROM notes"
+	notes = mysql.query_db(query)
+	return render_template('index.html', notes=notes)
+
+@app.route('/desc/<id>', methods=['POST'])
+def description(id):
+	query = "UPDATE notes (description, updated_at) VALUES (:description, NOW()) WHERE id = :id LIMIT 1"
+	data = {
+		'description': request.form['description'],
+		'id': id
+	}
+	return redirect('/')
+
+@app.route('/update')
+def update():
+	query = "SELECT * FROM notes"
+	notes = mysql.query_db(query)
+	return jsonify(notes=notes)
 
 
+@app.route('/new', methods=['POST'])
+def new():
+	data = {
+		'title': request.form['title']
 
+	}
+	query = "INSERT INTO notes (title, created_at, updated_at) VALUES (:title, NOW(), NOW())"
 
+	mysql.query_db(query, data)
+	return redirect('/')
 
+@app.route('/delete/<id>')
+def delete(id):
+	query = "DELETE FROM notes WHERE id = :id LIMIT 1"
+	data = {
+		'id': id
+	}
+	mysql.query_db(query, data)
+	return redirect('/update')
 
 
 
